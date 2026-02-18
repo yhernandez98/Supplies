3adc67269108afa9f429d9db74eb1b3295f53ff3
Yonatan Hernandez <yhernandez@supplies.com.co>
2026-02-18 09:27:47 -0500

    22

--- crm_sales_supplies/wizard/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import purchase_quotation_wizard
-from . import purchase_alert_validation_wizard
-from . import crm_lead_purchase_alert_wizard
-from . import sale_order_request_quotation_wizard
-from . import purchase_alert_manual_quotation_wizard
-from . import leasing_contract_wizard
-
--- subscription_licenses/wizard/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import license_equipment_add_multiple_wizard
-from . import license_add_multiple_warning_wizard
-from . import license_equipment_delete_warning_wizard
-from . import license_quantity_warning_wizard
