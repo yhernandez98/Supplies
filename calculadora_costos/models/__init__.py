e695caee43c4b474648c2f510dabef59fa4bc3ec
Yonatan Hernandez <yhernandez@supplies.com.co>
2026-02-25 10:23:29 -0500

    Cambios

--- calculadora_costos/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import parametros_financieros
-from . import calculadora_line
-from . import calculadora
-
--- mesa_ayuda_inventario/wizard/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import maintenance_order_wizard
-from . import add_equipment_wizard
-from . import equipment_change_wizard
-from . import activity_assignment_wizard
-from . import request_element_wizard
-from . import escalate_ticket_wizard
-
